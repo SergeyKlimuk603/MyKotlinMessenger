c5e8d918e439d8f85fb558f125c2b77be79545a0
Sergey Klimuk <sergey_klimuk@mail.ru>
2021-05-13 21:33:28 +0300

    feat: 7 add menu to fragment_settings.xml

--- app/src/main/java/by/klimuk/mykotlinmessenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/by/klimuk/mykotlinmessenger/ui/fragments/SettingsFragment.kt
@@ -1,17 +1,19 @@
 package by.klimuk.mykotlinmessenger.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import by.klimuk.mykotlinmessenger.R
 import by.klimuk.mykotlinmessenger.databinding.FragmentSettingsBinding
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
     }
 }
